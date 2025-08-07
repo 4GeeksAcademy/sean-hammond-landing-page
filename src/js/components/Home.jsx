86dc5cb7ba70024684871a04f8d8811a9f2e6de1
Sean-Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2025-08-07 00:15:24 +0000

    fixed several syntax errors

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,66 @@
 import React from "react";
+import Nav from "./Nav";
+import PageTitle from "./PageTitle";
+import Cards from "./Cards";
+
+const cardTitles = ["Card1", "Card2", "Card3", "Card4"];
+const cardTitlesPro = [
+  {
+    title: cardTitles[0],
+    description: cardTitles[0] + " description",
+    img: "https://picsum.photos/200/300?random=1",
+    buttonText: "card1 Button",
+    buttonAddress: "url",
+  },
+  {
+    title: cardTitles[1],
+    description: cardTitles[1] + " description",
+    img: "https://picsum.photos/200/300?random=2",
+    buttonText: "card2 Button",
+    buttonAddress: "url",
+  },
+  {
+    title: cardTitles[2],
+    description: cardTitles[2] + " description",
+    img: "https://picsum.photos/200/300?random=3",
+    buttonText: "card3 Button",
+    buttonAddress: "url",
+  },
+  {
+    title: cardTitles[3],
+    description: cardTitles[3] + " description",
+    img: "https://picsum.photos/200/300?random=4",
+    buttonText: "card4 Button",
+    buttonAddress: "url",
+  },
+];
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  return (
+    <div>
+      <Nav />
+      <PageTitle />
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+      <div className="d-flex flex-wrap">
+        {cardTitlesPro.map((cards, i) => {
+          return (
+            <Cards
+              key={i}
+              title={cards.title}
+              description={cards.description}
+              img={cards.img}
+              buttonText={cards.buttonText}
+              buttonAddress={cards.buttonAddress}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/js/components/PageTitle.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Nav from "./Nav.jsx";
+
+const PageTitle = () => {
+  return (
+    <div>
+	  <div className="p-5 mb-4 bg-body-tertiary rounded-3"> <div className="container-fluid py-5"> <h1 className="display-5 fw-medium">A Warm Welcome!</h1> <p className="col-md-8 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum earum a omnis deserunt reiciendis voluptatibus est vitae? Quia repellendus suscipit id doloremque dolorum possimus nobis soluta! Dolore amet saepe aperiam?</p> <button className="btn btn-primary btn-lg" type="button">Call to action!</button> </div> </div>
+    </div>
+  );
+};
+
+export default PageTitle
